8bc05e9817deb51d04a9cb1b3bac21fc001be410
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2022-06-29 12:33:22 +0530

    CORS error handle

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const filesRoute = require('./routes/files');
 const downloadsRoute = require('./routes/downloads');
 const saveRoute = require('./routes/save');
 
+//CORS Setting
+const CorsOptions = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+};
+  
+app.use(cors(CorsOptions));
 app.set('view engine', 'ejs');
 app.set('views',__dirname+'/views');
 app.use(express.static('public'));
